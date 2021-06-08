8ae5fed2789c07d7d830b039f7a772874b6cd4b1
Your Name <you@example.com>
2021-06-08 03:23:14 +0100

    accounts router built

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,63 @@
 const router = require('express').Router()
 
+const {
+  checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId,
+} = require('./accounts-middleware');
+
+const Account = require('./accounts-model');
+
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Account.get(req.query.length)
+  .then(account => {
+    res.status(200).json(account);
+  })
+  .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', checkAccountId, (req, res, next) => {
+  res.json(req.account)
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
+  const postAccount = { ...req.body};
+  Account.insert(postAccount)
+    .then(account => {
+      res.status(201).json(account);
+    })
+    .catch(error => {
+      next(error)
+    });
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', checkAccountPayload, checkAccountId, checkAccountNameUnique, (req, res, next) => {
+  Account.update(req.params.id, req.body)
+  .then(account => {
+    res.status(200).json(account);
+  })
+  .catch(error => {
+    next(error)
+  });
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  Account.remove(req.params.id)
+  .then(() => {
+    res.status(200).json({ message: 'The account has been nuked' });
+  })
+  .catch(next);
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  console.log('err handling middleware kicking in!', err.message)
+  res.status(err.status || 500).json({
+    custom: 'something exploded inside the app',
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 module.exports = router;
+
+
